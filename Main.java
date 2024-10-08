6b5ebfdc176eb73c7842fe7fb8b13001c459f2ff
Junaid Umar <152009831+Umaralp@users.noreply.github.com>
2024-10-08 23:13:40 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("my first JAVA program");
+        System.out.println("Hello World");
+
+    }
+}
+
